c13a2fbda11aee5d4b35a4606bc107f3c9d68d29
prashantmalik3008 <139915459+prashantmalik3008@users.noreply.github.com>
2024-10-21 13:42:49 +0530

    Project Initialised

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this YouTube sentiment analysis project, the goal is to analyze all the comments on a given YouTube video and classify them into positive, negative, and neutral sentiments. The analysis involves collecting comments, preprocessing the data by cleaning and standardizing the text, and then applying natural language processing (NLP) techniques to determine the sentiment behind each comment. A sentiment analysis model is used to categorize the comments, and the final output is a summary that provides insights into the overall sentiment of the audience toward the video.',
+    author='Prashant Malik',
+    license='',
+)
